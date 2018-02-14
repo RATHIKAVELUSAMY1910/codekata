e8675261ccb80d14ea6cbe4270ca55c4bf59a6ba
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-02-14 15:16:19 +0530

    Create odd.cpp

---
+++ odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cout<<"enter the number"<<endl;;
+	cin>>a;
+	if(a%2==0)
+	{
+		cout<<"the num is even";
+	}
+	else
+	{
+		cout<<"the num is odd";
+	}
+}
