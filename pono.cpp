04dccc3df89f0d1994946658b6225d0f3e9493d1
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-02-14 15:06:32 +0530

    Create pono.cpp

---
+++ pono.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cout<<"enter the number"<<endl;;
+	cin>>a;
+	if(a>0)
+	
+	{
+		cout<<"the number is positive"<<endl;
+		
+	}
+	else if(a<0)
+	{
+		cout<<"the num is negative"<<endl;
+	}
+	else
+	{
+		cout<<"the num is zero"<<endl;
+	}
+	return 0;
+}
