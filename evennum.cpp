582d40d4433a3434bb61c0876ca5793a29a1bc5b
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-02-15 14:16:20 +0530

    Create evennum.cpp

---
+++ evennum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,num;
+    cout<<"enter the a,b value"<<endl;
+    cin>>a>>b;
+      while(a<=b)
+      {
+      if(a%2 == 0)
+      cout <<a<<"is odd"<< endl;
+      a++;	
+      }
+ return 0;
+
+}
