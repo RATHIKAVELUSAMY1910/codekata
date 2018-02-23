3ef6719da36455774dfb7d894ce4ae647f6aef8b
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-02-23 17:07:20 +0530

    Create k string.cpp

---
+++ k string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string str;
+	cout<<"enter the string"<<endl;
+	cin>>str;
+	int k;
+	cout<<"enter the number"<<endl;
+	cin>>k;
+	for(int i=0;i<k;i++)
+	{
+	cout<<str<<endl;	
+	}
+	return 0;
+}
