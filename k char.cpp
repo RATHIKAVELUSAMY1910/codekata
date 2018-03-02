26a070f4c5815de2e54b5b40e3b2cdbf98c4ee84
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-02 20:50:37 +0530

    Create k char.cpp

---
+++ k char.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a,i;
+char b[20];
+cout<<"Enter the string"<<endl;
+cin>>b;
+cout<<"Enter the number"<<endl;
+cin>>a;
+for(i=0;i<a;i++)
+{
+cout<<b[i];
+}
+return 0;
+}
