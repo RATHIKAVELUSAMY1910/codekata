dc64299812779a5fe31cb1516cc5ce85e86a8351
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-02 21:08:11 +0530

    Create odd or even 2.cpp

---
+++ odd or even 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a,b,c;
+  cout<<"Enter the number"<<endl;
+      cin>>a>>b;
+      c=a+b;
+      if(c%2==0)
+      {
+          cout<<"Even"<<endl;
+      }
+      else
+      {
+          cout<<"Odd"<<endl;
+      }
+      return 0;
+}
