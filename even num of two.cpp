f43963799884d005f9a7d43b0c83fe520c90f308
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-02 10:45:26 +0530

    Create even num of two.cpp

---
+++ even num of two.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a,b,c;
+  cout<<"Enter the a and b value"<<endl;
+  cin>>a>>b;
+  c=a*b;
+  if(c%2==0)
+    {
+     cout<<"The num is even";
+    }
+  else
+    {
+      cout<<"The num is odd";
+   }
+  return 0;
+}
