59942fbf7a31197265d0585184d03b93a8e50c5c
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-04 20:23:47 +0530

    Create rangenum.cpp

---
+++ rangenum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+int num,a,b;
+ cout<<"enter a number"<<endl;
+ cin>>num;
+cout<<"enter the ranges"<<endl;
+ cin>>a>>b;
+ if((num>a)&&(num<b))
+ {
+  cout<<"yes";
+  }
+  else
+  {
+  cout<<"no";
+  }
+  return 0;
+  }
