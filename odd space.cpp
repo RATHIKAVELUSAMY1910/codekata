53c5c881d64dc161ded145e0529c607f31e58733
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-04 21:58:14 +0530

    Update odd space.cpp

--- odd space.cpp
+++ odd space.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-    int a[40],i,n;
-    cout<<"number:";
-    cin>>n;
-    for(i=0;i<n;i++)
+    int a[20],i,num;
+    cout<<"enter the number";
+    cin>>num;
+    for(i=0;i<num;i++)
     {
         cin>>a[i];
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<num;i++)
     {
         if(a[i]%2==0)
         {
