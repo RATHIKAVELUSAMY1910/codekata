cd219af14509752a9d77925079a8aa9092efbf6b
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-04 21:39:46 +0530

    Create 13 mul.cpp

---
+++ 13 mul.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a;
+  cout<<"Enter the number"<<endl;
+      cin>>a;
+      if(a%13==0)
+      {
+          cout<<"Yes";
+      }
+      else
+      {
+          cout<<"No";
+      }
+  return 0;
+    }
