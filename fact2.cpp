d20f19300b0b596d2f74475a197acc4d1927e267
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-05 19:57:13 +0530

    Create fact2.cpp

---
+++ fact2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,fact=1;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+    fact=fact*i;
+    }
+cout<<"the fact num is"<<fact<<endl;
+return 0;
+}
