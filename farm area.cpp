99c8bb21967982723734ad592e93fd2c55d2c141
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-07 17:08:27 +0530

    Create farm area.cpp

---
+++ farm area.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   float length,breath,area;
+   cout<<"enter the length"<<endl;
+   cin>>length;
+   cout<<"enter the breadth"<<endl;
+   cin>>breath;
+   area=length*breath;
+   cout<<area;
+   return 0;
+}
