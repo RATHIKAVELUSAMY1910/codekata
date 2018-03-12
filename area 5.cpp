3f03016fac662b7e270b5ceadaf5b84d8c8b2313
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-12 20:21:17 +0530

    Create area 5.cpp

---
+++ area 5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   float l,b,area;
+   cout<<"length"<<end;
+   cin>>len;
+   cout<<"breadth"<<endl;
+   cin>>b;
+   area=l*b;
+   cout<<area;
+   return 0;
+}
