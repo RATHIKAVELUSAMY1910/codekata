08b036487c4248601dba45fd684aff1ec7eb9729
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-15 18:20:33 +0530

    Create temp to cel.cpp

---
+++ temp to cel.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int temperature,celsius=0;
+    cout<<"enter the temperature"<<endl;
+    cin>>temperature;
+    celsius=temperature+273;
+    cout<<"the celsius is"<<" "<<celsius;
+}
