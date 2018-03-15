a064b33de32989e256d24505006960d6fc1a501d
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-15 18:04:08 +0530

    Create calc area.cpp

---
+++ calc area.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int l,b,h,volume,area;
+    cin>>l>>b>>h;
+    volume=l*b*h;
+    area=l*b;
+    cout<<"volume of cuboid"<<" "<<volume<<endl;
+    cout<<"total surface area"<<" "<<area<<endl;
+}
