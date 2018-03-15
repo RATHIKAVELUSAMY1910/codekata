e24403208f093ca04dd61453747202c3684b89ad
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-15 18:39:12 +0530

    Create floor val.cpp

---
+++ floor val.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float P,T,R,F;
+    cout<<"enter the amount"<<endl;
+    cin>>P;
+    cout<<"enter the time"<<endl;
+    cin>>T;
+    cout<<"enter the rate"<<endl;
+    cin>>R;
+    F=(P*T*R)/100;
+    cout<<"the floor value is"<<" "<<F;
+}
