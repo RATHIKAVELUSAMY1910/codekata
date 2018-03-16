455fed5f4eff1c7249973ac3e699ea31ca689e28
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-16 09:43:49 +0530

    Create 2 div.cpp

---
+++ 2 div.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i,rem;
+	cout<<"enter the number"<<endl;
+	cin>>n;
+	if(n%2==0)
+	    {  
+	    rem=n/2;
+	    cout<<rem<<endl;
+	    }
+	    else
+	    {
+	        cout<<n<<endl;
+	    }
+	
+	return 0;
+}
