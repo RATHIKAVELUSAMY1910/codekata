f944ff4e54e2a99e0bfb905b03155a1c7d6ced44
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-18 21:13:52 -0700

    Create alpahapet number.cpp

---
+++ alpahapet number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num;
+    char a=65;
+    cout<<"enter the number"<<endl;
+   cin>>num;
+   for(int i=1;i<num;i++)
+   {
+    a++;
+} 
+cout<<a;
+}
