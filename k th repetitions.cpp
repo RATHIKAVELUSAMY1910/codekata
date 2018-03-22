c4fae3940f4457592a81f93636f0238001b8dfed
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-22 21:03:14 +0530

    Create k th repetitions.cpp

---
+++ k th repetitions.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k,a[60];
+    cout<<"enter the n and k value"<<endl;
+    cin>>n>>k;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+         j++;
+        }
+    }
+    cout<<j;
+    return 0;
+}
