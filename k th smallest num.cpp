8e142122a854dd1976bf5f2b9bdc4e3a8de58355
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-03-22 20:49:44 +0530

    Create k th smallest num.cpp

---
+++ k th smallest num.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k,a[60];
+    cout<<"enter th n and k value"<<endl;
+    cin>>n>>k;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+ 
+        {
+            cout<<a[k-1];
+        }
+    return 0;
+}
