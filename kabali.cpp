c599bf16e84a76019cbfbd72663dbcdfc904b13d
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-05-24 16:13:31 +0530

    Create kabali.cpp

---
+++ kabali.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int a,b,c;
+   cout<<"Enter the two inputs";
+      cin>>a>>b;
+      if(a<b)
+      {
+         c=b-a;
+         cout<<"The result is<<c;
+      }
+      else
+      {
+          cout<<"a is greater than b"<<endl;
+      }
+ 
+    return 0;
+}
