afdb388bc1289b002552ad7b5748ada6cff861b1
RATHIKAVELUSAMY1910 <36355156+RATHIKAVELUSAMY1910@users.noreply.github.com>
2018-05-24 16:05:11 +0530

    Update count space.cpp

--- count space.cpp
+++ count space.cpp
@@ -1,38 +1,20 @@
 #include <iostream>
 using namespace std;
-class NoOfSpaces
+void main()
 {
-int p,a,d,i,ar[1000],c=0;
+int p,d,i,ar[1000],c=0;
 string s;
-public:
-NoOfSpaces()
-{
-get();
-count();
-display();
-}
-void get()
-{
 cout<<"INPUT"<<endl;
 getline(cin,s);
-}
-void count()
-{
 p=s.length();
 for(i=0;i<p;i++)
 {
 if(s[i]==' ')
-{c++;}
-}
-}
-void display()
 {
-cout<<"OUTPUT"<<endl;
+  c++;
+}
+}
 cout<<c;
 }
-};
-int main()
-{
-NoOfSpaces nc;
 return 0;
 }
